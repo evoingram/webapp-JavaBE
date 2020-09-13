2878d52a1d1f108510ddcabae18182b276f411ca
Erica L. Ingram <inquiries@aquoco.co>
2020-09-13 12:31:03 -0700

    mailclass serviceimpl done

--- src/main/java/com/aquoco/starthere/controllers/MailClassController.java
+++ src/main/java/com/aquoco/starthere/controllers/MailClassController.java
@@ -1,7 +1,24 @@
 package com.aquoco.starthere.controllers;
 
+import com.aquoco.starthere.logging.Loggable;
+import com.aquoco.starthere.services.MailClassService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /*
  * mailclass table fields: mcid, mailclass, description
  */
+@Loggable
+@RestController
+@RequestMapping("/mailclasses")
 public class MailClassController {
+    private static final Logger logger = LoggerFactory.getLogger(MailClassController.class);
+
+    @Autowired
+    private MailClassService mcService;
+
+    
 }

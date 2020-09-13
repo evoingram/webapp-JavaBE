aba6228b102535488558565e53f7b412aac3c224
Erica L. Ingram <inquiries@aquoco.co>
2020-09-13 11:58:31 -0700

    mailclass model done

--- src/main/java/com/aquoco/starthere/models/MailClass.java
+++ src/main/java/com/aquoco/starthere/models/MailClass.java
@@ -1,4 +1,65 @@
 package com.aquoco.starthere.models;
 
-public class MailClass {
+import com.aquoco.starthere.logging.Loggable;
+
+import javax.persistence.*;
+
+@Loggable
+@Entity
+@Table(name = "mailclass")
+public class MailClass extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long mcid;
+
+    @Column(nullable = false,
+            unique = true)
+    private String mailclass;
+
+    @Column(nullable = false)
+    private String description;
+
+
+    public MailClass() {
+    }
+
+    public MailClass(String mailclass, String description) {
+
+        this.mailclass = mailclass;
+        this.description = description;
+
+    }
+
+    public long getMcid() {
+        return mcid;
+    }
+
+    public void setMcid(long mcid) {
+        this.mcid = mcid;
+    }
+
+    public String getMailclass() {
+        return mailclass;
+    }
+
+    public void setMailclass(String mailclass) {
+        this.mailclass = mailclass;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "MailClass{" +
+                "mcid=" + mcid +
+                ", mailclass='" + mailclass + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }

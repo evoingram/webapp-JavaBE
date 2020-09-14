b007748ffab53e3494c4600b7f2c1feb6a8e6a47
Erica L. Ingram <inquiries@aquoco.co>
2020-09-13 19:35:49 -0700

    examtype repo done

--- src/main/java/com/aquoco/starthere/repository/ExamTypeRepository.java
+++ src/main/java/com/aquoco/starthere/repository/ExamTypeRepository.java
@@ -1,7 +1,23 @@
 package com.aquoco.starthere.repository;
 
+import com.aquoco.starthere.models.ExamType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
 /*
  * examtypes table fields: eid, examination, ecode
  */
-public interface ExamTypeRepository {
+public interface ExamTypeRepository
+        extends PagingAndSortingRepository<ExamType, Long> {
+
+    ExamType findByExamination(String examination);
+
+    List<ExamType> findByExaminationContainingIgnoreCase(String examination, Pageable pageable);
+    
+    ExamType findByEcode(String ecode);
+
+    List<ExamType> findByEcodeContainingIgnoreCase(String ecode, Pageable pageable);
+
 }

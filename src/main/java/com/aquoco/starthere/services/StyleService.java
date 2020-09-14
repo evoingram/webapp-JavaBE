f8a415432416369afc5c2f882faad910ef1849e6
Erica L. Ingram <inquiries@aquoco.co>
2020-09-13 19:03:44 -0700

    style service done

--- src/main/java/com/aquoco/starthere/services/StyleService.java
+++ src/main/java/com/aquoco/starthere/services/StyleService.java
@@ -1,4 +1,26 @@
 package com.aquoco.starthere.services;
 
+import com.aquoco.starthere.models.Style;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface StyleService {
+
+    List<Style> findAll(Pageable pageable);
+
+    Style findStyleById(long id);
+
+    Style findByStylename(String stylename);
+
+    List<Style> findByStylenameContaining(String stylename, Pageable pageable);
+
+    void delete(long id, boolean isAdmin);
+
+    Style save(Style stylename, boolean isAdmin);
+
+    Style update(Style stylename,
+                         long id,
+                         boolean isAdmin);
+
 }

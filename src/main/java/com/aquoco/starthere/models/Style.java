70edea70f350a59c60ef3dd26c6c52c03a5c5b2c
Erica L. Ingram <inquiries@aquoco.co>
2020-09-13 19:02:38 -0700

    style model done

--- src/main/java/com/aquoco/starthere/models/Style.java
+++ src/main/java/com/aquoco/starthere/models/Style.java
@@ -1,4 +1,50 @@
 package com.aquoco.starthere.models;
 
-public class Style {
+import com.aquoco.starthere.logging.Loggable;
+
+import javax.persistence.*;
+
+@Loggable
+@Entity
+@Table(name = "stylenames")
+public class Style extends Auditable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long sid;
+
+    @Column(nullable = false,
+            unique = true)
+    private String stylename;
+
+
+    public Style() {
+    }
+
+    public Style(String stylename) {
+        this.stylename = stylename;
+    }
+
+    public long getSid() {
+        return sid;
+    }
+
+    public void setSid(long sid) {
+        this.sid = sid;
+    }
+
+    public String getStylename() {
+        return stylename;
+    }
+
+    public void setStylename(String stylename) {
+        this.stylename = stylename;
+    }
+
+    @Override
+    public String toString() {
+        return "Style{" +
+                "sid=" + sid +
+                ", stylename='" + stylename + '\'' +
+                '}';
+    }
 }

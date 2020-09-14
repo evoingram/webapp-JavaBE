c6c5064eeb431da111f6f4ac82ccaa4d05b16ffb
Erica L. Ingram <inquiries@aquoco.co>
2020-09-13 19:04:32 -0700

    style repo done

--- src/main/java/com/aquoco/starthere/repository/StyleRepository.java
+++ src/main/java/com/aquoco/starthere/repository/StyleRepository.java
@@ -1,4 +1,16 @@
 package com.aquoco.starthere.repository;
 
-public interface StyleRepository {
+import com.aquoco.starthere.models.Style;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface StyleRepository
+        extends PagingAndSortingRepository<Style, Long> {
+
+    Style findByStylename(String stylename);
+
+    List<Style> findByStylenameContainingIgnoreCase(String stylename, Pageable pageable);
+    
 }
